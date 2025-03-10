71f1f48188ae162e3c5d2ea2c8530ef3bab3e9e1
matheus <matheusrodrig16@gmail.com>
2025-03-10 18:06:27 -0300

    feat: colors in numpy format

--- bot_tetris.py
+++ bot_tetris.py
@@ -1,3 +1,19 @@
 from locate_pixelcolor_cpppragma import search_colors
 import numpy as np
 import numexpr
+from fast_ctypes_screenshots import(
+    ScreenshotOfOneMonitor,
+)
+from mousekey import MouseKey
+
+mkey = MouseKey()
+mkey.enable_failsafekill('ctrl+e')
+
+# blue green red no numpy inverte
+green = 42, 127, 25 
+light_blue = 44,156,251 
+dark_blue = 34,62,141 
+
+r_green = np.array([list(reversed(green))], dtype=np.uint8)
+r_light_blue = np.array([list(reversed(light_blue))], dtype=np.uint8)
+r_dark_blue = np.array([list(reversed(dark_blue))], dtype=np.uint8)
